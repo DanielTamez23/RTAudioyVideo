e8fb0a4f1b4391be62512920fce5635c69e63d1c
DanielTamez23 <23032002Dd>
2025-04-03 15:30:28 -0600

    jiji

--- RTAudioyVideo.py
+++ RTAudioyVideo.py
@@ -53,7 +53,7 @@ def get_gmail_service():
             }
 
             flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
